9d93e3006470408cfaab103ed23738a5ce9126eb
Radhika <radhikaiyer@Radhikas-MacBook-Air.local>
2018-04-26 18:30:43 -0700

    Updated Game

--- app /routing/htmlRoutes.js
+++ app /routing/htmlRoutes.js
@@ -1,1 +1,16 @@
-# Friend-Finder-App
+//Dependencies
+var path = require('path');
+
+//Routing Functions
+
+module.exports = function(app){
+
+	app.get('/survey', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
+
+	app.use(function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+
+}
