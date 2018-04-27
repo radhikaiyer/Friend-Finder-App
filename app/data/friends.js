f34fb3e1efe3b47bb12e2b102043e860a8244704
Radhika <radhikaiyer@radhikas-air.attlocal.net>
2018-04-27 14:13:02 -0700

    Final Updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-{
+[{
     "name":"Radhika",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
@@ -13,5 +13,5 @@
         4,
         1
       ]
-  }
+  }]
   
--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // Add the application routes
-require('./app/routing/api-routes.js')(app); 
-require('./app/routing/html-routes.js')(app);
+require('./app/routing/api-routes')(app); 
+require('./app/routing/html-routes')(app);
 
 // Start listening on PORT
 app.listen(PORT, function() {
-  console.log('Friend Finder app is listening on PORT: ' + PORT);
+  console.log('App listening on PORT: ' + PORT);
 });
